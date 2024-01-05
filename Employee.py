27c8f7e358c4a8263b36e92a9c60fa416412f21c
Charilaos Alkiviades Savoullis <casavoullis@gmail.com>
2024-01-05 11:44:00 +0000

    fixed fullname class, also added setup class to remove redundant code in test class

--- Employee.py
+++ Employee.py
@@ -14,7 +14,7 @@ class Employee:
 
     @property
     def fullname(self):
-        return '{}.{}'.format(self.first, self.last)
+        return '{} {}'.format(self.first, self.last)
 
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amt)
