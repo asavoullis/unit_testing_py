9b1a54ae52f387e138c3c212efce2ccc66ba718e
Charilaos Alkiviades Savoullis <casavoullis@gmail.com>
2025-01-19 21:11:00 +0000

    added src file and new code to test

---
+++ src/slap_that_like_button.py
@@ -0,0 +1,69 @@
+import enum
+
+
+# class Color(enum.Enum):
+#     RED = 1
+#     GREEN = 2
+#     BLUE = 3
+
+
+class LikeState(enum.Enum):
+    empty = enum.auto()
+    liked = enum.auto()
+    disliked = enum.auto()
+
+
+slap_like_transitions = {
+    LikeState.empty: LikeState.liked,
+    LikeState.liked: LikeState.empty,
+    LikeState.disliked: LikeState.liked,
+}
+
+slap_dislike_transitions = {
+    LikeState.empty: LikeState.disliked,
+    LikeState.liked: LikeState.disliked,
+    LikeState.disliked: LikeState.empty,
+}
+
+
+def slap_like(state: LikeState) -> LikeState:
+    return slap_like_transitions[state]
+
+
+def slap_dislike(state: LikeState) -> LikeState:
+    return slap_dislike_transitions[state]
+
+
+def slaps_many(state: LikeState, slaps: str) -> LikeState:
+    for slap in slaps:
+        slap = slap.lower()
+        if slap == "l":
+            state = slap_like(slap)
+        elif slap == "d":
+            state = slap_dislike(slap)
+        else:
+            raise ValueError("invalid slap")
+    return state
+
+
+def main():
+    # # Moved Enum member access and comparison inside main()
+    # print(Color.RED)  # Color.RED
+    # print(Color.GREEN.name)  # 'GREEN'
+    # print(Color.BLUE.value)  # 3
+    # print()
+
+    # if Color.RED == Color(1):
+    #     print("Colors match!")  # Will print this because 1 corresponds to Color.RED
+
+    # print()
+    example = LikeState
+    print(example.empty)
+    print(example.empty.name)
+    print(example.empty.value)
+    print(example.liked.value)
+    print(type(example.empty))
+
+
+if __name__ == "__main__":
+    main()
