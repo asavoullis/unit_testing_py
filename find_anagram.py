11e824cdd939a00ceb092f94bbe544e78d7ac7f3
Charilaos Alkiviades Savoullis <casavoullis@gmail.com>
2025-01-20 21:11:00 +0000

    removed unecessary print statements

--- find_anagram.py
+++ find_anagram.py
@@ -7,11 +7,12 @@ of another word or phrase, using all the original letters exactly once.
 Your program should output whether the given words are anagrams or not.
 """
 
-with open('word_file.txt', 'r') as f:
+with open("word_file.txt", "r") as f:
     words = f.readlines()
 
 # for word in words:
 #     print(word)
+
 
 def find_anagrams(word):
     hashmap = {}
@@ -24,10 +25,8 @@ def find_anagrams(word):
     return hashmap
 
 
-
 steak_hash = find_anagrams("steak")
+# print(steak_hash)
 for word in words:
     if find_anagrams(word.lower().rstrip()) == steak_hash:
         print(word)
-
-
